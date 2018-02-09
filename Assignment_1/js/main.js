45da2361062e7f15ced91ab42f634a1e2de3e1b7
cfrench5 <36209475+cfrench5@users.noreply.github.com>
2018-02-09 17:06:04 -0500

    commit 2
    commit 2

--- Assignment_1/js/main.js
+++ Assignment_1/js/main.js
@@ -13,19 +13,16 @@ window.onload = function () {
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
     function preload() {
-
-        game.load.image('breakout', 'assets/breakout.png');
-        game.load.image('starfield', 'assets/starfield.jpg');
-
+        game.load.atlas('breakout', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
+        game.load.image('starfield', 'assets/misc/starfield.jpg');
     }
 
     var ball;
     var paddle;
-    var bricks;
 
     var ballOnPaddle = true;
 
-    var lives = 3;
+    var lives = 1;
     var score = 0;
 
     var scoreText;
@@ -42,20 +39,6 @@ window.onload = function () {
         game.physics.arcade.checkCollision.down = false;
 
         s = game.add.tileSprite(0, 0, 800, 600, 'starfield');
-
-        bricks = game.add.group();
-        bricks.enableBody = true;
-        bricks.physicsBodyType = Phaser.Physics.ARCADE;
-
-        var brick;
-
-        for (var y = 0; y < 4; y++) {
-            for (var x = 0; x < 15; x++) {
-                brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y + 1) + '_1.png');
-                brick.body.bounce.set(1);
-                brick.body.immovable = true;
-            }
-        }
 
         paddle = game.add.sprite(game.world.centerX, 500, 'breakout', 'paddle_big.png');
         paddle.anchor.setTo(0.5, 0.5);
@@ -107,7 +90,7 @@ window.onload = function () {
         }
         else {
             game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
-            game.physics.arcade.collide(ball, bricks, ballHitBrick, null, this);
+
         }
 
     }
@@ -151,34 +134,6 @@ window.onload = function () {
 
     }
 
-    function ballHitBrick(_ball, _brick) {
-
-        _brick.kill();
-
-        score += 10;
-
-        scoreText.text = 'score: ' + score;
-
-        //  Are they any bricks left?
-        if (bricks.countLiving() == 0) {
-            //  New level starts
-            score += 1000;
-            scoreText.text = 'score: ' + score;
-            introText.text = '- Next Level -';
-
-            //  Let's move the ball back to the paddle
-            ballOnPaddle = true;
-            ball.body.velocity.set(0);
-            ball.x = paddle.x + 16;
-            ball.y = paddle.y - 16;
-            ball.animations.stop();
-
-            //  And bring the bricks back from the dead :)
-            bricks.callAll('revive');
-        }
-
-    }
-
     function ballHitPaddle(_ball, _paddle) {
 
         var diff = 0;
@@ -187,16 +142,19 @@ window.onload = function () {
             //  Ball is on the left-hand side of the paddle
             diff = _paddle.x - _ball.x;
             _ball.body.velocity.x = (-10 * diff);
+            Score += 1;
         }
         else if (_ball.x > _paddle.x) {
             //  Ball is on the right-hand side of the paddle
             diff = _ball.x - _paddle.x;
             _ball.body.velocity.x = (10 * diff);
+            Score += 1;
         }
         else {
             //  Ball is perfectly in the middle
             //  Add a little random X to stop it bouncing straight up!
             _ball.body.velocity.x = 2 + Math.random() * 8;
+            Score += 1;
         }
 
     }
