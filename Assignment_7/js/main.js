045941d3ab3212e6994fc2618b06bb070741f498
cfrench5 <36209475+cfrench5@users.noreply.github.com>
2018-04-13 12:32:17 -0400

    commit 12

--- Assignment_7/js/main.js
+++ Assignment_7/js/main.js
@@ -125,11 +125,11 @@ window.onload = function() {
 	    textp1.text = p1score;
 	    textp2.text = p2score;
 
-	    if((p1score >= 4 && p1.x == 100 && p1.y == 100) || (p1score >= 4 && p1.x == p2.x && p1.y == p2.y){
+	    if((p1score >= 4 && p1.x == 100 && p1.y == 100) || (p1score >= 4 && p1.x == p2.x && p1.y == p2.y)){
 	    	st = 100;
 	    	text.text = "Player 1 won!";
 	    }
-	    if((p2score >= 4 && p2.x == 700 && p2.y == 500) || (p2score >= 4 && p2.x == p1.x && p2.y == p1.y){
+	    if((p2score >= 4 && p2.x == 700 && p2.y == 500) || (p2score >= 4 && p2.x == p1.x && p2.y == p1.y)){
 	    	st = 100;
 	    	text.text = "Player 2 won!";
 	    }
