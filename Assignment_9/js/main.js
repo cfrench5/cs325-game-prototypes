07b203800d7d2de3ca80f84323c5f4e33b7d142e
cfrench5 <36209475+cfrench5@users.noreply.github.com>
2018-05-04 21:17:01 -0400

    commit 2

--- Assignment_9/js/main.js
+++ Assignment_9/js/main.js
@@ -3,7 +3,7 @@
 
 window.onload = function() {
 
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+	var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', {
 		preload : preload,
 		create : create,
 		update: update,
@@ -21,6 +21,7 @@ window.onload = function() {
 	    game.load.spritesheet('explode', 'assets/explode.png', 64, 64, 25);
 	    game.load.audio('sound', 'assets/explode.mp3');
 	    game.load.audio('bgm', 'assets/bgm.mp3');
+	    game.load.audio('shield', 'assets/shield.mp3');
 	}
 
 	var start;
@@ -34,6 +35,7 @@ window.onload = function() {
 	var style;
 	var text;
 	var text2;
+	var text3;
 	var map;
 	var map2;
 	var ctr;
@@ -45,10 +47,17 @@ window.onload = function() {
 	var powerRdy;
 	var power;
 	var playerDead;
+	var masterTimer;
+	var fxShield;
+	var playOnce;
 
 	function create() {
 	    game.physics.startSystem(Phaser.Physics.ARCADE);
 
+	    masterTimer = 0;
+	    playOnce = false;
+
+	    fxShield = game.add.audio('shield');
 	    fx = game.add.audio('sound');
 	    bgm = game.add.audio('bgm');
 	    map = game.add.sprite(400, 300, 'map');
@@ -84,6 +93,9 @@ window.onload = function() {
 	    text2 = game.add.text(400, 180, "", style);
 	    text2.anchor.x = 0.5;
 	    text2.anchor.y = 0.5;
+	    text3 = game.add.text(53, 60, "", style);
+	    text3.anchor.x = 0.5;
+	    text3.anchor.y = 0.5;
 
 	    state = 1;
 	    ctr = 0;
@@ -92,12 +104,34 @@ window.onload = function() {
 	    power = false;
 	    playerDead = false;
 	    random = game.rnd.between(20, 30);
+
+	    //player.body.width = 25;
+	    //player.body.height = 25;
+	    player.body.setSize(40, 40, 5, 5);
 	}
 
 	function update() {
 	    game.physics.arcade.collide(player, redBullets);
+
 	    if (state == 2)
 	    {
+	        text3.text = "" + Math.floor(masterTimer / 60);
+	        text3.bringToTop();
+
+	        if (playerDead)
+	        {
+	            text2.text = "You died! You Lasted " + Math.floor(masterTimer / 60) + " seconds! Press ENTER to restart!";
+                
+	            if (game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+	                bgm.stop();
+	                game.state.restart();
+	            }
+	        }
+
+	        else
+	        {
+	            masterTimer++;
+	        }
 	        
 	        ctr++;
 	        redBullets.forEach(checkPos, this);
@@ -117,7 +151,6 @@ window.onload = function() {
 	        if (game.input.activePointer.leftButton.isDown && powerRdy)
 	        {
 	            ctr2 = 0;
-                
 	        }
 
 	        if (!powerRdy)
@@ -125,6 +158,13 @@ window.onload = function() {
 	            if (ctr2 < 50 && !playerDead)
 	            {
 	                power = true;
+                    
+	                if (!playOnce)
+	                {
+	                    fxShield.play();
+	                    playOnce = true;
+	                }
+
 	                shield.x = player.x;
 	                shield.y = player.y;
 	            }
@@ -132,6 +172,7 @@ window.onload = function() {
 	            else
 	            {
 	                power = false;
+	                playOnce = false;
 	                shield.x = 1000;
 	                shield.y = 1000;
 	            }
@@ -230,7 +271,44 @@ window.onload = function() {
 	        }
 	    }
 	}
+    /*
+    function createTimer(){
+ 
+        var me = this;
+ 
+        me.timeLabel = me.game.add.text(me.game.world.centerX, 100, "00:00", {font: "100px Arial", fill: "#fff"});
+        me.timeLabel.anchor.setTo(0.5, 0);
+        me.timeLabel.align = 'center';
+ 
+    }
 
+    function updateTimer(){
+ 
+        var me = this;
+ 
+        var currentTime = new Date();
+        var timeDifference = me.startTime.getTime() - currentTime.getTime();
+ 
+        //Time elapsed in seconds
+        me.timeElapsed = Math.abs(timeDifference / 1000);
+ 
+        //Time remaining in seconds
+        var timeRemaining = me.timeElapsed;
+ 
+        //Convert seconds into minutes and seconds
+        var minutes = Math.floor(timeRemaining / 60);
+        var seconds = Math.floor(timeRemaining) - (60 * minutes);
+ 
+        //Display minutes, add a 0 to the start if less than 10
+        var result = (minutes < 10) ? "0" + minutes : minutes;
+ 
+        //Display seconds, add a 0 to the start if less than 10
+        result += (seconds < 10) ? ":0" + seconds : ":" + seconds;
+ 
+        me.timeLabel.text = result;
+ 
+    }
+    */
 	function startButtonExec() {
 	    state = 2;
 	    bgm.play();
@@ -257,6 +335,7 @@ window.onload = function() {
 	    explode.animations.play('bam', 30, false);
 	    fx.play();
 	}
+
 	function bulletExplode(p, s) {
 	    //text.text = "boom";
 	    s.kill();
@@ -267,9 +346,11 @@ window.onload = function() {
 	    fx.play();
 	}
 
-	function render() {
-
-	    game.debug.text('Time: ' + this.game.time.totalElapsedSeconds(), 32, 32);
-
+	function render()
+	{
+	    //player.sprite.body.width = 25;
+	    //player.sprite.body.height = 25;
+	    game.debug.body(player);
 	}
+
 }
